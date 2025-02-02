8c07e9e59b489e85934efd27be39091d858a2948
AliHassanzadeh79 <alihassan.gerashi@gmail.com>
2025-02-02 23:19:03 +0330

    create api random_number and weather

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('generate-password', views.generate_password)
+    path('generate-password', views.generate_password),
+    path('random-number', views.random_number),
+    path('weather', views.weather),
 ]
